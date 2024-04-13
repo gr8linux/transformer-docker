ecd1c3b0a4ec4b660112d51d01806472ca25247e
Mohammad Shams <mohammad.shams@solvians.com>
2024-04-13 13:10:23 +0200

    fix issue with flagged

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import gradio as gr
 from transformers import pipeline
-import os
 
 # Load your model
 model = pipeline("text-classification", model="distilbert-base-uncased-finetuned-sst-2-english")
@@ -9,23 +8,16 @@ def predict(text):
     results = model(text)
     return str(results)
 
+# Set up the Gradio interface with correct flagging directory
+flagging_directory = "/app/flagged"  # Ensuring the directory exists and is writable
 
-# Set a flagging directory in the user's home directory
-flagging_directory = "/home/myuser/flagged"
-
-# Ensure the directory exists
-os.makedirs(flagging_directory, exist_ok=True)
-# Set up the Gradio interface
 iface = gr.Interface(
     fn=predict,
     inputs="text",
     outputs="text",
     title="Text Classification",
-    flagging_options={"dir": flagging_directory}
+    flagging_dir=flagging_directory  # Correct parameter for specifying the flagging directory
 )
-
-
-#iface = gr.Interface(fn=predict, inputs="text", outputs="text", title="Text Classification")
 
 if __name__ == "__main__":
     iface.launch()
